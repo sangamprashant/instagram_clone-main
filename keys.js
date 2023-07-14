523a8e9af9269ded841237e319382b8d3ce558b0
Prashant Srivastav <srivastavp891@gmail.com>
2023-07-15 02:57:27 +0530

    velcel update

---
+++ keys.js
@@ -0,0 +1,6 @@
+module.exports ={
+    mongoUrl : "mongodb+srv://sangamprashant:ps998428@cluster0.xv21zrp.mongodb.net/?retryWrites=true&w=majority",
+  
+    Jwt_secret : "sangamprashantsrivastavshashi"
+  
+  };
